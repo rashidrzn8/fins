52cf6040b0ea5f94c8477bc78c662e03fca6462f
rashid <rashidrizan8@gamil.com>
2025-08-19 23:08:21 +0530

    added changes

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,10 @@ import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
-import About from "./pages/About";
-import Services from "./pages/Services";
-import Gallery from "./pages/Gallery";
-import Contact from "./pages/Contact";
+// import About from "./pages/About";
+// import Services from "./pages/Services";
+// import Gallery from "./pages/Gallery";
+// import Contact from "./pages/Contact";
 
 function App() {
   return (
@@ -15,10 +15,10 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
+          {/* <Route path="/about" element={<About />} />
           <Route path="/services" element={<Services />} />
           <Route path="/gallery" element={<Gallery />} />
-          <Route path="/contact" element={<Contact />} />
+          <Route path="/contact" element={<Contact />} /> */}
         </Routes>
         <Footer />
       </div>
